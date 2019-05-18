f1554ce0d437b56a974b5345a93084d3aa6283d0
Steve Fan <fanzc.daily@gmail.com>
2019-05-18 21:43:53 +0800

    add raw trade event model

--- src/model.rs
+++ src/model.rs
@@ -339,6 +339,43 @@ pub struct TradesEvent {
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
+pub struct RawTradeEvent {
+    #[serde(rename = "e")]
+    pub event_type: String,
+
+    #[serde(rename = "E")]
+    pub event_time: u64,
+
+    #[serde(rename = "s")]
+    pub symbol: String,
+
+    #[serde(rename = "t")]
+    pub trade_id: u64,
+
+    #[serde(rename = "p")]
+    pub price: String,
+
+    #[serde(rename = "q")]
+    pub qty: String,
+
+    #[serde(rename = "b")]
+    pub buyer_order_id: u64,
+
+    #[serde(rename = "a")]
+    pub seller_order_id: u64,
+
+    #[serde(rename = "T")]
+    pub trade_order_time: u64,
+
+    #[serde(rename = "m")]
+    pub is_buyer_maker: bool,
+
+    #[serde(skip_serializing, rename = "M")]
+    pub m_ignore: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
 pub struct DayTickerEvent {
     #[serde(rename = "e")] pub event_type: String,
 
